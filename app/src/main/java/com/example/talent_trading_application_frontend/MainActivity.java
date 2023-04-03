c75ab18c1862a3abe9e30c6fd80b9f04246beff2
Han <hek416@naver.com>
2023-04-03 21:25:42 +0900

    04-03

--- app/src/main/java/com/example/talent_trading_application_frontend/MainActivity.java
+++ app/src/main/java/com/example/talent_trading_application_frontend/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.talent_trading_application_frontend.MemberFunction.LoginActivity;
 import com.example.talent_trading_application_frontend.MemberFunction.RegisterMember;
@@ -16,8 +17,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button_register = (Button) findViewById(R.id.Register_Button);
-        Button button_login = (Button) findViewById(R.id.Login_Button);
+        ImageButton button_register = (ImageButton) findViewById(R.id.Register_Button);
+        ImageButton button_login = (ImageButton) findViewById(R.id.Login_Button);
+
+        ImageButton button_chat = (ImageButton) findViewById(R.id.chat_button);
+
+        ImageButton button_board = (ImageButton) findViewById(R.id.board_button);
         button_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
